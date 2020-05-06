37908b40a2844d1be811319c4676329eac04d9a9
danstonebuilt <dnansel@hotmail.com>
2020-05-06 20:15:28 -0300

    060520202015

--- Javascript/src/index.js
+++ Javascript/src/index.js
@@ -1,10 +1,12 @@
 import * as ex from './map-sample.js'
+import * as el from './manipulating-dom-elements.js'
 
 
 //const ul = document.createElement('ul');
 //const li = document.createElement('li');
 
 let root = document.querySelector('#root');
+const ul = document.createElement('ul');
 
 /*
 ex.listaModelosCarros.forEach(modelo => {
@@ -12,6 +14,19 @@ ex.listaModelosCarros.forEach(modelo => {
 });
 */
 
+/*
 ex.listaCarrosFiltrados.forEach(carro => {
     root.innerHTML += `<p>${carro.model}</p>`
 });
+*/
+
+
+
+ex.listaModelosCarros.forEach(carro => {
+    const li = document.createElement('li');
+    li.innerText = carro;
+    li.classList.add('Carros');
+    ul.appendChild(li);
+});
+
+root.appendChild(ul);
